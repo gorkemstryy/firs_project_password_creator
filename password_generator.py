404d432e8358d681f7744823648d6053f54044cf
Sudo <gorkemmsarac@gmail.com>
2026-05-16 20:32:58 +0300

    password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,13 +1,15 @@
-import random as rn
+import random as rn 
 
 
 def passwd():
-        word ="abcdefghjiklmnopqrstuvwxyz"
-        upword="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        bd = "!@#$%^&*"
-        
-
+        word ="abcdefghjiklmnopqrstuvwxyz"    #digits for random to choice 
+        upword="ABCDEFGHIJKLMNOPQRSTUVWXYZ"     #capslock digits for random to choice
+        bd = "!@#$%^&*"         #idk either
         print(rn.choice(upword) + rn.choice(bd) + rn.choice(bd)+ rn.choice(upword) + rn.choice(word) + rn.choice(upword) + rn.choice(bd) + rn.choice(upword) + str(rn.randint(1,101)) + rn.choice(word) + str(rn.randint(1,101)) + rn.choice(word) + rn.choice(upword) + rn.choice(upword) + str(rn.randint(1,101)))
-i = int(input("How many strong passwords you wanna get? "))
+        #this prints passwords generates 18 character long passwords
+
+i = int(input("How many strong passwords you wanna get? ")) #how many passwd u want 
 for sayi in range(i):
         passwd()
+
+        
